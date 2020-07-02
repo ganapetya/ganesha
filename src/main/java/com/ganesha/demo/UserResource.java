b584b67ba48f718a11dcda4c6324cdd79d9c44b8
tierhetze <peter.kovgan@gmail.com>
2020-07-02 11:47:18 +0300

    res

---
+++ src/main/java/com/ganesha/demo/UserResource.java
@@ -0,0 +1,30 @@
+package com.ganesha.demo;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Value;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+
+@Value
+public class UserResource {
+
+    @NotNull
+    private final String name;
+
+    @NotNull
+    private final String email;
+
+    private LocalDateTime registrationDate;
+
+    public UserResource(
+            @JsonProperty("name") String name,
+            @JsonProperty("email") String email) {
+        this.name = name;
+        this.email = email;
+        this.registrationDate = null;
+    }
+
+
+
+
+}
