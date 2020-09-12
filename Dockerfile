ed3f9ec12b9f68e1159edefc508b7aec199f40a8
peterkovgan <peter.kovgan@gmail.com>
2020-09-12 10:39:11 -0400

    added kubectl stuff

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM java:8-jdk-alpine
-COPY ./target/demo-0.0.1-SNAPSHOT.jar /home/peter/apps/app/
-WORKDIR /home/peter/apps/app
+ENV CURRENT_DIR .
+WORKDIR /app
+COPY $CURRENT_DIR/target/demo-0.0.1-SNAPSHOT.jar /app
 EXPOSE 8081
 ENTRYPOINT ["java", "-jar", "demo-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/ganesha/demo/DemoApplication.java
+++ src/main/java/com/ganesha/demo/DemoApplication.java
@@ -1,11 +1,20 @@
 package com.ganesha.demo;
 
+import com.google.protobuf.Api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1Pod;
+import io.kubernetes.client.openapi.models.V1PodList;
+import io.kubernetes.client.util.Config;
 
+import java.io.IOException;
 
 @SpringBootApplication
 @RestController
@@ -24,6 +33,19 @@ public class DemoApplication {
 	@PostMapping("/user")
 	public String sendUser(@Valid @RequestBody UserResource userResource) {
 		return String.format("Hello %s!", userResource.getName());
+	}
+
+	@GetMapping("/getPods")
+	public String getPods() throws IOException, ApiException {
+		ApiClient client = Config.defaultClient();
+		Configuration.setDefaultApiClient(client);
+        StringBuilder sb = new StringBuilder();
+		CoreV1Api api = new CoreV1Api();
+		V1PodList list = api.listNamespacedPod("default",null,null,null,null,null,null,null,null,null);
+		for (V1Pod item : list.getItems()) {
+			sb.append(item.getMetadata().getName());
+		}
+		return String.format("Hello %s!", sb.toString());
 	}
 
 }
